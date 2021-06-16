34a8ef985d9e8924fd358258380e1f30d2dd3e52
Paulo <paulo.henrbraguim@gmail.com>
2021-06-15 21:03:47 -0300

    Alteração das cores

--- MentoriaDevSti3/Views/Principal.xaml.cs
+++ MentoriaDevSti3/Views/Principal.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,17 +8,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MentoriaDevSti3
+namespace MentoriaDevSti3.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Lógica interna para Principal.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Principal : Window
     {
-        public MainWindow()
+        public Principal()
         {
             InitializeComponent();
         }
